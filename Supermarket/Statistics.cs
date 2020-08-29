fa887086268ce03cd0bf7c993aa7f407695ef0a7
Anna Demchuk <anidemchuk@gmail.com>
2020-08-29 19:47:02 +0300

    Create class Statistics

---
+++ Supermarket/Statistics.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket
+{
+  public  class Statistics
+    {
+        public List<Registr> statisticList;
+    }
+}
