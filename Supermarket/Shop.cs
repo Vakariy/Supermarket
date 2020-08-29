9d39231c040528d46ac1f8228cae404c42a0050d
Anna Demchuk <anidemchuk@gmail.com>
2020-08-29 17:29:04 +0300

    Create Shop.FirstDelivery() and PrintShopProduct()

--- Supermarket/Shop.cs
+++ Supermarket/Shop.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Supermarket
 {
-   public class Shop
+    public class Shop
     {
         public List<Product> prodactListShop = new List<Product>();
         public List<Buyer> buyerList = new List<Buyer>();
@@ -12,9 +12,44 @@ namespace Supermarket
 
         public DateTime dateInShop;
 
+        public void FirstDelivery()
+        {
+            Random random = new Random();
+            //  prodactListShop = stock.stockProductList;
+            for (int i = 0; i < stock.stockProductList.Count; i++)
+            {
+                prodactListShop.Add(stock.stockProductList[i]);
+                prodactListShop[i].quantity = random.Next(3, 5);
+            }
+        }
+
+
+        public void PrintShopProduct()
+        {
+            Console.WriteLine("--Print catalog products of supermarket--");
+            Console.WriteLine("------------------------");
+
+            foreach (var item in prodactListShop)
+            {
+                Console.WriteLine($"Name       : {item.name}");
+                Console.WriteLine($"Price      : {item.price} grn");
+                Console.WriteLine($"Quantity   : {item.quantity}");
+                Console.WriteLine($"Weight     : {item.weight} gr");
+                Console.WriteLine($"Shelf      : {item.numberShelf}");
+                Console.WriteLine($"Days stored: {item.daysStored} days");
+                Console.WriteLine($"Date       : {item.dateStartStored}");
+                Console.WriteLine("------------------------");
+            }
+        }
+
         public void Start()
         {
+            FirstDelivery();
 
+            // Показать наличие продуктов в супермеркете.
+            PrintShopProduct();
         }
+
+
     }
 }
