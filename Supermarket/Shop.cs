54b9a52cc83a952b16c00a227d4aa0662211503c
Anna Demchuk <anidemchuk@gmail.com>
2020-08-29 15:34:16 +0300

    Create class Shop

---
+++ Supermarket/Shop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket
+{
+   public class Shop
+    {
+        public List<Product> prodactListShop = new List<Product>();
+        public List<Buyer> buyerList = new List<Buyer>();
+
+        public DateTime dateInShop;
+
+        public void Start()
+        {
+
+        }
+    }
+}
