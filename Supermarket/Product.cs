11633d4c16dfa20cae73f8e277a081e157282d3d
Anna Demchuk <anidemchuk@gmail.com>
2020-08-29 10:43:59 +0300

    Create class Prodect end fieds

---
+++ Supermarket/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket
+{
+public class Product
+    {
+        public string name;
+        public int price;
+        public int quantity;
+
+        // Вес продуктв в грамммах, нужен для полок.
+        public int weight;
+
+        // Срок годности в днях.
+        public int daysStored;
+
+        // День началао хранения.
+        public DateTime dateStartStored;
+
+    }
+}
