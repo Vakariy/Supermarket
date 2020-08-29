4146cc0b93309664425d5751883fa98ad2ab4abd
Anna Demchuk <anidemchuk@gmail.com>
2020-08-29 19:39:39 +0300

    Create class  Registr

---
+++ Supermarket/Registr.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket
+{
+  public  class Registr
+    {
+        public DateTime day;
+        public Product product;
+    }
+}
