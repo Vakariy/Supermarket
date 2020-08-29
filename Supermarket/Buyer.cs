cf2cba5234154b095b91bd26ce5d3f780235ea5b
Anna Demchuk <anidemchuk@gmail.com>
2020-08-29 14:00:18 +0300

    Create class Buyer

---
+++ Supermarket/Buyer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket
+{
+  public  class Buyer
+    {
+        public List<Product> buyerProductList = new List<Product>();
+    }
+}
